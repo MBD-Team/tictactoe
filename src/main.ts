cd61a2052d8b6d672fd2183601a0d0e881b3f26b
tim-labahn <tim.labahn@freaknet.de>
2023-08-11 12:20:54 +0200

    changePlayer()

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,11 @@
-import './style.css'
+import './style.css';
+let player = 1;
+
+function changePlayer() {
+  if (player === 2) {
+    player = 1;
+  }
+  if (player === 1) {
+    player = 2;
+  }
+}
